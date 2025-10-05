bb7f8f0aab35aa433c6c29189a8417bc605bae37
priya <haripriya.godishala12@gmail.com>
2025-10-05 13:09:51 +0530

    docket

--- install-docker.sh
+++ install-docker.sh
@@ -15,7 +15,6 @@ N="\e[0m"
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-chmod -aG docker ec2-user
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
